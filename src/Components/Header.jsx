cb27d8d86c8fd73b80d107afec531d4c1b8149cd
Adil Shaikh <shaikhadil881@gmail.com>
2023-03-18 12:22:07 +0100

    changes

--- src/Components/Header.jsx
+++ src/Components/Header.jsx
@@ -1,12 +1,16 @@
 import { Button } from "@mui/material";
 import React from "react";
 import "./header.css";
+import "./Form";
+import Form from "./Form";
 
 const Header = () => {
   return (
     <>
       <div className="masterNavBar">
-        <Button variant="contained">Post Job</Button>
+        <Button variant="contained" size="large">
+          Post Job
+        </Button>
         &nbsp; &nbsp;
         <Button variant="contained">Log In</Button>
         &nbsp;&nbsp;
